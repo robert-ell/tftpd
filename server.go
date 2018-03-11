6ac7292b7a007850f9b97186cb382eb1a92c54a3
Robert Ell <ell.train@gmail.com>
2018-03-11 03:16:16 -0700

    Vim settings masked spacing incosistencies.

--- server.go
+++ server.go
@@ -4,7 +4,7 @@ import (
     "fmt"
     "net"
     "os"
-//	"reflect"
+//    "reflect"
 )
 
 /* A Simple function to verify error */
@@ -16,12 +16,12 @@ func CheckError(err error) {
 }
 
 func StartServer() {
-	f, err := os.Create("/tmp/requests.log")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	StartLogger(f, os.Stdout, os.Stderr)
+    f, err := os.Create("/tmp/requests.log")
+    if err != nil {
+        panic(err)
+    }
+    defer f.Close()
+    StartLogger(f, os.Stdout, os.Stderr)
     ServerAddr,err := net.ResolveUDPAddr("udp",":10069")
     CheckError(err)
 
@@ -31,15 +31,15 @@ func StartServer() {
 
 
     for {
-		buf := make([]byte, MaxPacketSize)
+        buf := make([]byte, MaxPacketSize)
         _,addr,_ := ServerConn.ReadFromUDP(buf)
 
-		// Parse the packet, declaring the struct implementation, setting fields appropriately
-		packet, err := ParsePacket(buf)
-		// Get the response from the handling of the packet
-		response := packet.Handle(addr)
+        // Parse the packet, declaring the struct implementation, setting fields appropriately
+        packet, err := ParsePacket(buf)
+        // Get the response from the handling of the packet
+        response := packet.Handle(addr)
 
-		ServerConn.WriteToUDP(response, addr)
+        ServerConn.WriteToUDP(response, addr)
 
         if err != nil {
             fmt.Println("Error: ",err)
--- wire.go
+++ wire.go
@@ -1,10 +1,10 @@
 package tftp
 
 import (
-	"bytes"
-	"encoding/binary"
-	"errors"
-	"fmt"
+    "bytes"
+    "encoding/binary"
+    "errors"
+    "fmt"
     "net"
 )
 
@@ -13,11 +13,11 @@ import (
 const MaxPacketSize = 2048
 
 const (
-	OpRRQ   uint16 = 1
-	OpWRQ          = 2
-	OpData         = 3
-	OpAck          = 4
-	OpError        = 5
+    OpRRQ   uint16 = 1
+    OpWRQ          = 2
+    OpData         = 3
+    OpAck          = 4
+    OpError        = 5
 )
 
 var Buffers = make(map[string][]byte)
@@ -25,10 +25,10 @@ var ServerData = make(map[string][]byte)
 
 // packet is the interface met by all packet structs
 type Packet interface {
-	// Parse parses a packet from its wire representation
-	Parse([]byte) error
-	// Serialize serializes a packet to its wire representation
-	Serialize() []byte
+    // Parse parses a packet from its wire representation
+    Parse([]byte) error
+    // Serialize serializes a packet to its wire representation
+    Serialize() []byte
     // Return uint16 op code of tftp packet
     GetOp() uint16
     // Handle the packet, returning a buffer to send to the receiver
@@ -55,9 +55,9 @@ func getBlock(buffer string, num uint16) ([]byte) {
 // RRQ/  | 01/02 |  Filename  |   0  |    Mode    |   0  |
 // WRQ    -----------------------------------------------
 type PacketRequest struct {
-	Op       uint16 // OpRRQ or OpWRQ
-	Filename string
-	Mode     string
+    Op       uint16 // OpRRQ or OpWRQ
+    Filename string
+    Mode     string
 }
 
 func (p *PacketRequest) Handle(addr *net.UDPAddr) ([]byte) {
@@ -101,24 +101,24 @@ func (p *PacketRequest) GetOp() (op uint16) {
 }
 
 func (p *PacketRequest) Parse(buf []byte) (err error) {
-	if p.Op, buf, err = parseUint16(buf); err != nil {
-		return err
-	}
-	if p.Filename, buf, err = parseString(buf); err != nil {
-		return err
-	}
-	if p.Mode, buf, err = parseString(buf); err != nil {
-		return err
-	}
-	return nil
+    if p.Op, buf, err = parseUint16(buf); err != nil {
+        return err
+    }
+    if p.Filename, buf, err = parseString(buf); err != nil {
+        return err
+    }
+    if p.Mode, buf, err = parseString(buf); err != nil {
+        return err
+    }
+    return nil
 }
 
 func (p *PacketRequest) Serialize() []byte {
-	buf := make([]byte, 2+len(p.Filename)+1+len(p.Mode)+1)
-	binary.BigEndian.PutUint16(buf, p.Op)
-	copy(buf[2:], p.Filename)
-	copy(buf[2+len(p.Filename)+1:], p.Mode)
-	return buf
+    buf := make([]byte, 2+len(p.Filename)+1+len(p.Mode)+1)
+    binary.BigEndian.PutUint16(buf, p.Op)
+    copy(buf[2:], p.Filename)
+    copy(buf[2+len(p.Filename)+1:], p.Mode)
+    return buf
 }
 
 // PacketData carries a block of data in a file transmission.
@@ -127,12 +127,12 @@ func (p *PacketRequest) Serialize() []byte {
 // DATA  | 03    |   Block #  |    Data    |
 //        ---------------------------------
 type PacketData struct {
-	BlockNum uint16
-	Data     []byte
+    BlockNum uint16
+    Data     []byte
 }
 
 func (p *PacketData) Handle(addr *net.UDPAddr) ([]byte) {
-	i := bytes.IndexByte(p.Data, 0)
+    i := bytes.IndexByte(p.Data, 0)
     Buffers[addr.String()] = append(Buffers[addr.String()], p.Data[:i]...)
     if i < 512 {
         filename, buf, _ := parseString(Buffers[addr.String()])
@@ -149,20 +149,20 @@ func (p *PacketData) GetOp() (op uint16) {
 }
 
 func (p *PacketData) Parse(buf []byte) (err error) {
-	buf = buf[2:] // skip over op
-	if p.BlockNum, buf, err = parseUint16(buf); err != nil {
-		return err
-	}
-	p.Data = buf
-	return nil
+    buf = buf[2:] // skip over op
+    if p.BlockNum, buf, err = parseUint16(buf); err != nil {
+        return err
+    }
+    p.Data = buf
+    return nil
 }
 
 func (p *PacketData) Serialize() []byte {
-	buf := make([]byte, 4+len(p.Data))
-	binary.BigEndian.PutUint16(buf, OpData)
-	binary.BigEndian.PutUint16(buf[2:], p.BlockNum)
-	copy(buf[4:], p.Data)
-	return buf
+    buf := make([]byte, 4+len(p.Data))
+    binary.BigEndian.PutUint16(buf, OpData)
+    binary.BigEndian.PutUint16(buf[2:], p.BlockNum)
+    copy(buf[4:], p.Data)
+    return buf
 }
 
 // PacketAck acknowledges receipt of a data packet
@@ -172,7 +172,7 @@ func (p *PacketData) Serialize() []byte {
 //        --------------------
 
 type PacketAck struct {
-	BlockNum uint16
+    BlockNum uint16
 }
 
 func (p *PacketAck) Handle(addr *net.UDPAddr) ([]byte) {
@@ -189,18 +189,18 @@ func (p *PacketAck) GetOp() (op uint16) {
 }
 
 func (p *PacketAck) Parse(buf []byte) (err error) {
-	buf = buf[2:] // skip over op
-	if p.BlockNum, buf, err = parseUint16(buf); err != nil {
-		return err
-	}
-	return nil
+    buf = buf[2:] // skip over op
+    if p.BlockNum, buf, err = parseUint16(buf); err != nil {
+        return err
+    }
+    return nil
 }
 
 func (p *PacketAck) Serialize() []byte {
-	buf := make([]byte, 4)
-	binary.BigEndian.PutUint16(buf, OpAck)
-	binary.BigEndian.PutUint16(buf[2:], p.BlockNum)
-	return buf
+    buf := make([]byte, 4)
+    binary.BigEndian.PutUint16(buf, OpAck)
+    binary.BigEndian.PutUint16(buf[2:], p.BlockNum)
+    return buf
 }
 
 // PacketError is sent by a peer who has encountered an error condition
@@ -209,8 +209,8 @@ func (p *PacketAck) Serialize() []byte {
 // ERROR | 05    |  ErrorCode |   ErrMsg   |   0  |
 //        ----------------------------------------
 type PacketError struct {
-	Code uint16
-	Msg  string
+    Code uint16
+    Msg  string
 }
 
 func (p *PacketError) Handle(addr *net.UDPAddr) ([]byte) {
@@ -223,52 +223,52 @@ func (p *PacketError) GetOp() (op uint16) {
 }
 
 func (p *PacketError) Parse(buf []byte) (err error) {
-	buf = buf[2:] // skip over op
-	if p.Code, buf, err = parseUint16(buf); err != nil {
-		return err
-	}
-	if p.Msg, buf, err = parseString(buf); err != nil {
-		return err
-	}
-	return nil
+    buf = buf[2:] // skip over op
+    if p.Code, buf, err = parseUint16(buf); err != nil {
+        return err
+    }
+    if p.Msg, buf, err = parseString(buf); err != nil {
+        return err
+    }
+    return nil
 }
 
 func (p *PacketError) Serialize() []byte {
-	buf := make([]byte, 4+len(p.Msg)+1)
-	binary.BigEndian.PutUint16(buf, OpError)
-	binary.BigEndian.PutUint16(buf[2:], p.Code)
-	copy(buf[4:], p.Msg)
-	return buf
+    buf := make([]byte, 4+len(p.Msg)+1)
+    binary.BigEndian.PutUint16(buf, OpError)
+    binary.BigEndian.PutUint16(buf[2:], p.Code)
+    copy(buf[4:], p.Msg)
+    return buf
 }
 
 // parseUint16 reads a big-endian uint16 from the beginning of buf,
 // returning it along with a slice pointing at the next position in the buffer.
 func parseUint16(buf []byte) (uint16, []byte, error) {
-	if len(buf) < 2 {
+    if len(buf) < 2 {
         Error.Printf("Packet Truncated.  Packet: %s", string(buf))
-		return 0, nil, errors.New("packet truncated")
-	}
-	return binary.BigEndian.Uint16(buf), buf[2:], nil
+        return 0, nil, errors.New("packet truncated")
+    }
+    return binary.BigEndian.Uint16(buf), buf[2:], nil
 }
 
 // parseString reads a null-terminated ASCII string from buf,
 // returning it along with a slice pointing at the next position in the buffer.
 func parseString(buf []byte) (string, []byte, error) {
-	i := bytes.IndexByte(buf, 0)
-	if i < 0 {
+    i := bytes.IndexByte(buf, 0)
+    if i < 0 {
         Error.Printf("Packet Truncated.  Packet: %s", string(buf))
-		return "", nil, errors.New("packet truncated")
-	}
-	return string(buf[:i]), buf[i+1:], nil
+        return "", nil, errors.New("packet truncated")
+    }
+    return string(buf[:i]), buf[i+1:], nil
 }
 
 // ParsePacket parses a packet from its wire representation.
 func ParsePacket(buf []byte) (p Packet, err error) {
-	var opcode uint16
-	if opcode, _, err = parseUint16(buf); err != nil {
-		return
-	}
-	switch opcode {
+    var opcode uint16
+    if opcode, _, err = parseUint16(buf); err != nil {
+        return
+    }
+    switch opcode {
         case OpRRQ, OpWRQ:
             p = &PacketRequest{}
         case OpData:
@@ -280,7 +280,7 @@ func ParsePacket(buf []byte) (p Packet, err error) {
         default:
             err = fmt.Errorf("unexpected opcode %d", opcode)
             return
-	}
-	err = p.Parse(buf)
-	return
+    }
+    err = p.Parse(buf)
+    return
 }
